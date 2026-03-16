21cd90194b1910074fd20ac00e20053815a640b0
Imari <ahmadsecondacc@gmail.com>
2026-03-16 10:57:48 +0800

    new orbit mode for camera movement and rotation

---
+++ build/CMakeFiles/Iris.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/ahmadzuhri/CLionProjects/Iris_Fresh/imgui/backends/imgui_impl_sdl2.cpp" "CMakeFiles/Iris.dir/imgui/backends/imgui_impl_sdl2.cpp.o" "gcc" "CMakeFiles/Iris.dir/imgui/backends/imgui_impl_sdl2.cpp.o.d"
+  "/Users/ahmadzuhri/CLionProjects/Iris_Fresh/imgui/backends/imgui_impl_sdlrenderer2.cpp" "CMakeFiles/Iris.dir/imgui/backends/imgui_impl_sdlrenderer2.cpp.o" "gcc" "CMakeFiles/Iris.dir/imgui/backends/imgui_impl_sdlrenderer2.cpp.o.d"
+  "/Users/ahmadzuhri/CLionProjects/Iris_Fresh/imgui/imgui.cpp" "CMakeFiles/Iris.dir/imgui/imgui.cpp.o" "gcc" "CMakeFiles/Iris.dir/imgui/imgui.cpp.o.d"
+  "/Users/ahmadzuhri/CLionProjects/Iris_Fresh/imgui/imgui_demo.cpp" "CMakeFiles/Iris.dir/imgui/imgui_demo.cpp.o" "gcc" "CMakeFiles/Iris.dir/imgui/imgui_demo.cpp.o.d"
+  "/Users/ahmadzuhri/CLionProjects/Iris_Fresh/imgui/imgui_draw.cpp" "CMakeFiles/Iris.dir/imgui/imgui_draw.cpp.o" "gcc" "CMakeFiles/Iris.dir/imgui/imgui_draw.cpp.o.d"
+  "/Users/ahmadzuhri/CLionProjects/Iris_Fresh/imgui/imgui_tables.cpp" "CMakeFiles/Iris.dir/imgui/imgui_tables.cpp.o" "gcc" "CMakeFiles/Iris.dir/imgui/imgui_tables.cpp.o.d"
+  "/Users/ahmadzuhri/CLionProjects/Iris_Fresh/imgui/imgui_widgets.cpp" "CMakeFiles/Iris.dir/imgui/imgui_widgets.cpp.o" "gcc" "CMakeFiles/Iris.dir/imgui/imgui_widgets.cpp.o.d"
+  "/Users/ahmadzuhri/CLionProjects/Iris_Fresh/main.cpp" "CMakeFiles/Iris.dir/main.cpp.o" "gcc" "CMakeFiles/Iris.dir/main.cpp.o.d"
+  "/Users/ahmadzuhri/CLionProjects/Iris_Fresh/src/application.cpp" "CMakeFiles/Iris.dir/src/application.cpp.o" "gcc" "CMakeFiles/Iris.dir/src/application.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
